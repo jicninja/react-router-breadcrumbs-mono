f16d1b232d7b1e3981d4713fb3bb3ceaf999d933
Ignacio Castro <ignaciocastro@Ignacios-MacBook-Pro.local>
2025-06-28 16:32:11 -0300

    install components

--- apps/router-web/app/app.tsx
+++ apps/router-web/app/app.tsx
@@ -1,7 +1,31 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@rr/ui';
 
 export function App() {
   return (
     <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
       <h1 className="text-3xl font-bold underline">
         Welcome to the Router Web App!
       </h1>
---
+++ packages/ui/src/lib/ui.tsx
@@ -0,0 +1,9 @@
+export function RrUi() {
+  return (
+    <div>
+      <h1>Welcome to RrUi!</h1>
+    </div>
+  );
+}
+
+export default RrUi;
